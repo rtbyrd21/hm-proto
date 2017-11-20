eb0a587865a69bda5b21faba81b9c1d27200c43e
Rob Byrd <byrd.t.rob@gmail.com>
2017-11-20 10:51:02 -0500

    first commit

--- brooklyn/settings_local.py
+++ brooklyn/settings_local.py
@@ -2,17 +2,17 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'heroku_0639161351e83ae',
-#         'USER': 'bf2906c2b0cbc2',
-#         'PASSWORD': '86c85ba9',
-#         'HOST': 'us-cdbr-iron-east-04.cleardb.net',
-#         'PORT': '3306',
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroku_0639161351e83ae',
+        'USER': 'bf2906c2b0cbc2',
+        'PASSWORD': '86c85ba9',
+        'HOST': 'us-cdbr-iron-east-04.cleardb.net',
+        'PORT': '3306',
+    }
+}
 
 # DATABASES = {
 #     'default': {
