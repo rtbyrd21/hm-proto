02e334142b9f658399734db4deede8c63bc05b10
Rob Byrd <byrd.t.rob@gmail.com>
2017-11-20 10:30:35 -0500

    first commit

--- brooklyn/settings.py
+++ brooklyn/settings.py
@@ -70,36 +70,36 @@ WSGI_APPLICATION = 'brooklyn.wsgi.application'
 
 
 # Register database schemes in URLs.
-urlparse.uses_netloc.append('mysql')
-
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print 'Unexpected error:', sys.exc_info()
+# urlparse.uses_netloc.append('mysql')
+#
+# try:
+#
+#     # Check to make sure DATABASES is set in settings.py file.
+#     # If not default to {}
+#
+#     if 'DATABASES' not in locals():
+#         DATABASES = {}
+#
+#     if 'DATABASE_URL' in os.environ:
+#         url = urlparse.urlparse(os.environ['DATABASE_URL'])
+#
+#         # Ensure default database exists.
+#         DATABASES['default'] = DATABASES.get('default', {})
+#
+#         # Update with environment configuration.
+#         DATABASES['default'].update({
+#             'NAME': url.path[1:],
+#             'USER': url.username,
+#             'PASSWORD': url.password,
+#             'HOST': url.hostname,
+#             'PORT': url.port,
+#         })
+#
+#
+#         if url.scheme == 'mysql':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+# except Exception:
+#     print 'Unexpected error:', sys.exc_info()
 
 
 
