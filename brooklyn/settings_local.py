a2b287f96150ede6aa2d1687602368b5cad8032c
Rob Byrd <byrd.t.rob@gmail.com>
2017-11-20 10:47:52 -0500

    first commit

--- brooklyn/settings_local.py
+++ brooklyn/settings_local.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
